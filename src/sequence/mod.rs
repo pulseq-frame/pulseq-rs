90969b8dc5849e70523973c496e0817a10e332c4
J-Endres <67545816+J-Endres@users.noreply.github.com>
2024-01-10 18:30:11 +0100

    implemented sequence validation

--- src/sequence/mod.rs
+++ src/sequence/mod.rs
@@ -19,7 +19,9 @@ pub struct Sequence {
 
 impl Sequence {
     pub fn from_parsed_file(sections: Vec<Section>) -> Result<Self, error::Error> {
-        Ok(from_raw::from_raw(sections)?)
+        let tmp = from_raw::from_raw(sections)?;
+        tmp.validate()?;
+        Ok(tmp)
     }
 
     pub fn from_source(source: &str) -> Result<Self, error::Error> {
@@ -29,6 +31,57 @@ impl Sequence {
     pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, error::Error> {
         let source = std::fs::read_to_string(path)?;
         Self::from_source(&source)
+    }
+
+    pub fn validate(&self) -> Result<(), error::Error> {
+        // NOTE: We could check if block IDs are in some order or at least not
+        // duplicated, but as they are never really used, this might be too strict
+
+        // Check if no event is longer than the duration of its block
+        for block in &self.blocks {
+            let check = |dur: Option<f32>, err| {
+                dur.map_or(Ok(()), |d| {
+                    if d > block.duration + f32::EPSILON {
+                        Err(err)
+                    } else {
+                        Ok(())
+                    }
+                })
+            };
+            let grad_raster = self.time_raster.grad;
+
+            check(
+                block.rf.as_ref().map(|rf| rf.duration(self.time_raster.rf)),
+                error::Error::ParseError(error::ParseError::Generic),
+            )?;
+            check(
+                block.gx.as_ref().map(|gx| gx.duration(grad_raster)),
+                error::Error::ParseError(error::ParseError::Generic),
+            )?;
+            check(
+                block.gy.as_ref().map(|gy| gy.duration(grad_raster)),
+                error::Error::ParseError(error::ParseError::Generic),
+            )?;
+            check(
+                block.gz.as_ref().map(|gz| gz.duration(grad_raster)),
+                error::Error::ParseError(error::ParseError::Generic),
+            )?;
+            check(
+                block.adc.as_ref().map(|adc| adc.duration()),
+                error::Error::ParseError(error::ParseError::Generic),
+            )?;
+        }
+
+        // Check things like identical shape size and no negative times
+        for block in &self.blocks {
+            block.rf.as_ref().map_or(Ok(()), |tmp| tmp.validate())?;
+            block.gx.as_ref().map_or(Ok(()), |tmp| tmp.validate())?;
+            block.gy.as_ref().map_or(Ok(()), |tmp| tmp.validate())?;
+            block.gz.as_ref().map_or(Ok(()), |tmp| tmp.validate())?;
+            block.adc.as_ref().map_or(Ok(()), |tmp| tmp.validate())?;
+        }
+
+        Ok(())
     }
 }
 
@@ -53,11 +106,6 @@ impl Default for TimeRaster {
         }
     }
 }
-
-// TODO: give all structs a validate() function, that checks if
-// - all shapes have the same size
-// - no event is longer than the block duration
-// - maybe other invariants the spec mandates?
 
 pub struct Block {
     /// Blocks are stored in a simple vector, instead of a HashMap with their ID
@@ -125,11 +173,23 @@ pub struct Adc {
 
 pub struct Shape(pub Vec<f32>);
 
-// Helper functions
+// Helper functions and other impls
 
 impl Rf {
     pub fn duration(&self, rf_raster: f32) -> f32 {
         self.delay + calc_shape_dur(&self.amp_shape, self.time_shape.as_deref(), rf_raster)
+    }
+
+    fn validate(&self) -> Result<(), error::Error> {
+        if self.phase_shape.0.len() != self.amp_shape.0.len() {
+            return Err(error::Error::ParseError(error::ParseError::Generic));
+        }
+        if let Some(time_shape) = &self.time_shape {
+            if time_shape.0.len() != self.amp_shape.0.len() {
+                return Err(error::Error::ParseError(error::ParseError::Generic));
+            }
+        }
+        Ok(())
     }
 }
 
@@ -148,11 +208,55 @@ impl Gradient {
             } => delay + rise + flat + fall,
         }
     }
+
+    fn validate(&self) -> Result<(), error::Error> {
+        match self {
+            Gradient::Free {
+                delay, shape, time, ..
+            } => {
+                if *delay < 0.0 {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else if time.as_ref().map_or(false, |t| shape.0.len() != t.0.len()) {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else {
+                    Ok(())
+                }
+            }
+            Gradient::Trap {
+                rise,
+                flat,
+                fall,
+                delay,
+                ..
+            } => {
+                if *rise < 0.0 {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else if *flat < 0.0 {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else if *fall < 0.0 {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else if *delay < 0.0 {
+                    Err(error::Error::ParseError(error::ParseError::Generic))
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }
 }
 
 impl Adc {
     pub fn duration(&self) -> f32 {
         self.delay + self.num as f32 * self.dwell
+    }
+    fn validate(&self) -> Result<(), error::Error> {
+        if self.dwell < 0.0 {
+            Err(error::Error::ParseError(error::ParseError::Generic))
+        } else if self.delay < 0.0 {
+            Err(error::Error::ParseError(error::ParseError::Generic))
+        } else {
+            Ok(())
+        }
     }
 }
 
