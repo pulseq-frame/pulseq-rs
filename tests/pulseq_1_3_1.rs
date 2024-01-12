52f904ed6c573bf3749ccc68b4dec29b77655cfd
J-Endres <67545816+J-Endres@users.noreply.github.com>
2024-01-12 20:31:08 +0100

    Fixed test: use unwrap to show actual error message

--- tests/pulseq_1_3_1.rs
+++ tests/pulseq_1_3_1.rs
@@ -2,33 +2,33 @@ use pulseq_rs::Sequence;
 
 #[test]
 fn epi_se_rs() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/epi_se_rs.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/epi_se_rs.seq").unwrap();
 }
 #[test]
 fn epi_se() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/epi_se.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/epi_se.seq").unwrap();
 }
 #[test]
 fn epi() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/epi.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/epi.seq").unwrap();
 }
 #[test]
 fn gre_label() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/gre_label.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/gre_label.seq").unwrap();
 }
 #[test]
 fn gre() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/gre.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/gre.seq").unwrap();
 }
 #[test]
 fn haste() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/haste.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/haste.seq").unwrap();
 }
 #[test]
 fn tse() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/tse.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/tse.seq").unwrap();
 }
 #[test]
 fn ute() {
-    assert!(Sequence::from_file("assets/1.3.1.post1/ute.seq").is_ok());
+    Sequence::from_file("assets/1.3.1.post1/ute.seq").unwrap();
 }
--- tests/pulseq_1_4_0.rs
+++ tests/pulseq_1_4_0.rs
@@ -2,45 +2,45 @@ use pulseq_rs::Sequence;
 
 #[test]
 fn epi_label() {
-    assert!(Sequence::from_file("assets/1.4.0/epi_label.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/epi_label.seq").unwrap();
 }
 #[test]
 fn epi_se_rs() {
-    assert!(Sequence::from_file("assets/1.4.0/epi_se_rs.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/epi_se_rs.seq").unwrap();
 }
 #[test]
 fn epi_se() {
-    assert!(Sequence::from_file("assets/1.4.0/epi_se.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/epi_se.seq").unwrap();
 }
 #[test]
 fn epi() {
-    assert!(Sequence::from_file("assets/1.4.0/epi.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/epi.seq").unwrap();
 }
 #[test]
 fn gre_label() {
-    assert!(Sequence::from_file("assets/1.4.0/gre_label.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/gre_label.seq").unwrap();
 }
 #[test]
 fn gre_radial() {
-    assert!(Sequence::from_file("assets/1.4.0/gre_radial.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/gre_radial.seq").unwrap();
 }
 #[test]
 fn gre() {
-    assert!(Sequence::from_file("assets/1.4.0/gre.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/gre.seq").unwrap();
 }
 #[test]
 fn haste() {
-    assert!(Sequence::from_file("assets/1.4.0/haste.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/haste.seq").unwrap();
 }
 #[test]
 fn mprage() {
-    assert!(Sequence::from_file("assets/1.4.0/mprage.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/mprage.seq").unwrap();
 }
 #[test]
 fn tse() {
-    assert!(Sequence::from_file("assets/1.4.0/tse.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/tse.seq").unwrap();
 }
 #[test]
 fn ute() {
-    assert!(Sequence::from_file("assets/1.4.0/ute.seq").is_ok());
+    Sequence::from_file("assets/1.4.0/ute.seq").unwrap();
 }
