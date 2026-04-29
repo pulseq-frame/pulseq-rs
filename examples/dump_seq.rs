60897ec339dde90997083dab9fdbe3a8903a8170
J-Endres <67545816+J-Endres@users.noreply.github.com>
2026-04-29 15:14:46 +0200

    update dump_seq example (formatting WIP)

--- examples/dump_seq.rs
+++ examples/dump_seq.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let seq = Sequence::from_file("assets/1.4.0/epi_se_rs.seq").unwrap();
-    let mut out = File::create("assets/epi_se_rs.seq.dump").unwrap();
+    let seq = Sequence::from_file("assets/flash_je.seq").unwrap();
+    let mut out = File::create("assets/flash_je.seq.dump").unwrap();
     write!(out, "{seq}").unwrap();
 }
